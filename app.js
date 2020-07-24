39e6d589c9a722e665aff2b0a891526583ec0300
hezhang18 <hezhang18@outlook.com>
2020-07-24 19:37:00 +0800

    update new

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const formidable = require('formidable');
 const fs = require('fs');
 const app = express();
 
@@ -54,6 +55,40 @@ app.get('/cache', (req, res)=>{
         res.send(result);
     }) 
 })
+
+// 12 页面请求接口, 需要使用 formidable
+// 1. FormData 的使用
+app.post('/formData', (req, res)=>{
+    // 创建 formidable 表单解析对象
+    const form = new formidable.IncomingForm();
+    // 解析客户端传过来的 FormData 对象
+    form.parse(req, (err, fields, files) => {
+        res.send(fields);
+    })
+})
+
+// 12 页面请求接口, 需要使用 formidable
+// 2. FormData 二进制文件上传
+app.post('/upload', (req, res)=>{
+    // 创建 formidable 表单解析对象
+    const form = new formidable.IncomingForm();
+    // 设置上传文件的存储路径
+    form.uploadDir = path.join(__dirname, 'public', 'uploads');
+    // 保留上传文件的后缀名
+    form.keepExtensions = true;
+    // 解析客户端传过来的 FormData 对象
+    form.parse(req, (err, fields, files) => {
+        // 2. FormData 二进制文件上传
+        // res.send('ok');
+        
+        // 3. 图片文件上传实时预览，attrName 来自前端代码：formData.append('attrName', this.files[0]);
+        res.send({
+            path: files.attrName.path.split('public')[1]
+        });
+    })
+})
+
+
 
 app.listen(3000);
 console.log('Server Start Success');
